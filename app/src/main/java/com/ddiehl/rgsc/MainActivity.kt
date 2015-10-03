a2a828a4a93deee8b604e06b4a30bbe96a427d13
damien5314 <damien5314@gmail.com>
2015-10-03 10:04:27 -0700

    Added tabs for navigation

--- app/src/main/java/com/ddiehl/rgsc/MainActivity.kt
+++ app/src/main/java/com/ddiehl/rgsc/MainActivity.kt
@@ -2,11 +2,22 @@ package com.ddiehl.rgsc
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
+import butterknife.bindView
 
 public class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    object MainActivity {
+        val TAG_ITG = "ITG"
+        val TAG_DDREX = "DDR_EXTREME"
+    }
+
+    private val tabs: TabLayout by bindView(R.id.tabs)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tabs.addTab(tabs.newTab().setText("ITG").setTag(MainActivity.TAG_ITG));
+        tabs.addTab(tabs.newTab().setText("DDR Ex").setTag(MainActivity.TAG_DDREX));
     }
 }
