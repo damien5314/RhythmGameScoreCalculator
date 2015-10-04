b4d2826d22bf0b8a01680bd103644c11e1e9f594
damien5314 <damien5314@gmail.com>
2015-10-04 11:24:28 -0700

    Set TabLayout color scheme to theme

--- app/src/main/java/com/ddiehl/rgsc/MainActivity.kt
+++ app/src/main/java/com/ddiehl/rgsc/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.ddiehl.rgsc
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import butterknife.bindView
 
-public class MainActivity : AppCompatActivity() {
-    object MainActivity {
-        val TAG_ITG = "ITG"
-        val TAG_DDREX = "DDR_EXTREME"
-    }
+const val TAB_ITG = "ITG"
+const val TAB_DDREX = "DDR_EXTREME"
 
-    private val tabs: TabLayout by bindView(R.id.tabs)
+public class MainActivity : AppCompatActivity() {
+    private val mTabs: TabLayout by bindView(R.id.tabs)
+    private val mViewPager: ViewPager by bindView(R.id.view_pager)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tabs.addTab(tabs.newTab().setText("ITG").setTag(MainActivity.TAG_ITG));
-        tabs.addTab(tabs.newTab().setText("DDR Ex").setTag(MainActivity.TAG_DDREX));
+        mTabs.addTab(mTabs.newTab().setText(R.string.tab_itg).setTag(TAB_ITG));
+        mTabs.addTab(mTabs.newTab().setText(R.string.tab_ddrex).setTag(TAB_DDREX));
     }
 }
